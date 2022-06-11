760ea85dfb1c17148d04476b3f0db1186460d17c
stanley <smveme@gmail.com>
2022-06-11 19:30:35 +0100

    [fix] autorisation des requetes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = require('./routes/matrix.routes')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
